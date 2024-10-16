07f3253aa21e8f3ed7b59db2a020a6e52f44b30b
celso <celso.sjunior@outlook.com>
2024-10-16 20:45:26 -0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,21 +1,21 @@
-Bun.serve({
-  port: process.env.PORT || 3000, // Porta dinâmica para se ajustar ao Vercel
-  fetch(req) {
+export default {
+  port: process.env.PORT || 3000, // Use a porta fornecida pela Vercel
+  fetch(req: Request) {
     const url = new URL(req.url);
     if (url.pathname === "/") {
       return new Response("Welcome to Bun Server running on Vercel!", {
         status: 200,
         headers: {
-          "Content-Type": "text/plain"
-        }
+          "Content-Type": "text/plain",
+        },
       });
     } else {
       return new Response("404 Not Found", {
         status: 404,
         headers: {
-          "Content-Type": "text/plain"
-        }
+          "Content-Type": "text/plain",
+        },
       });
     }
   },
-});
+};
