3e9051ecea5d70bac53ae5149994e89d9cd517b7
celso <celso.sjunior@outlook.com>
2024-10-16 23:17:28 -0300

    Create getmev2.py

---
+++ getmev2.py
@@ -0,0 +1,70 @@
+from requests_oauthlib import OAuth1Session
+import os
+from dotenv import load_dotenv
+import json
+
+# Carrega as variáveis do arquivo .env
+load_dotenv()
+
+consumer_key = os.environ.get("CONSUMER_KEY")
+consumer_secret = os.environ.get("CONSUMER_SECRET")
+
+# O restante do código continua igual
+fields = "created_at,description"
+params = {"user.fields": fields}
+
+# Get request token
+request_token_url = "https://api.twitter.com/oauth/request_token"
+oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
+
+try:
+    fetch_response = oauth.fetch_request_token(request_token_url)
+except ValueError:
+    print(
+        "Pode haver um problema com o consumer_key ou consumer_secret que você inseriu."
+    )
+
+resource_owner_key = fetch_response.get("oauth_token")
+resource_owner_secret = fetch_response.get("oauth_token_secret")
+print("Got OAuth token: %s" % resource_owner_key)
+
+# Get authorization
+base_authorization_url = "https://api.twitter.com/oauth/authorize"
+authorization_url = oauth.authorization_url(base_authorization_url)
+print("Por favor, vá aqui e autorize: %s" % authorization_url)
+verifier = input("Cole o PIN aqui: ")
+
+# Get the access token
+access_token_url = "https://api.twitter.com/oauth/access_token"
+oauth = OAuth1Session(
+    consumer_key,
+    client_secret=consumer_secret,
+    resource_owner_key=resource_owner_key,
+    resource_owner_secret=resource_owner_secret,
+    verifier=verifier,
+)
+oauth_tokens = oauth.fetch_access_token(access_token_url)
+
+access_token = oauth_tokens["oauth_token"]
+access_token_secret = oauth_tokens["oauth_token_secret"]
+
+# Make the request
+oauth = OAuth1Session(
+    consumer_key,
+    client_secret=consumer_secret,
+    resource_owner_key=access_token,
+    resource_owner_secret=access_token_secret,
+)
+
+response = oauth.get("https://api.twitter.com/2/users/me", params=params)
+
+if response.status_code != 200:
+    raise Exception(
+        "Request returned an error: {} {}".format(response.status_code, response.text)
+    )
+
+print("Response code: {}".format(response.status_code))
+
+json_response = response.json()
+
+print(json.dumps(json_response, indent=4, sort_keys=True))
